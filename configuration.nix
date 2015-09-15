9818470839aeee9aca9f7d4b45305b572c643c07
Chris Martin <ch.martin@gmail.com>
2015-09-14 21:58:54 -0700

    First version of nixos config :)

---
+++ configuration.nix
@@ -0,0 +1,94 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.initrd.luks.devices = [
+    { name = "root"; device = "/dev/sda3"; preLVM = true; }
+  ];
+  boot.loader.grub.device = "/dev/sda";
+
+  # Use the gummiboot efi boot loader.
+  boot.loader.gummiboot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    byobu dropbox fish git tmux tree
+
+    jmtpfs
+
+    keepassx synapse wpa_supplicant_gui unclutter xchat
+
+    chromium firefox
+
+    curl wget
+
+    emacs vim
+
+    gradle leiningen openjdk python ruby
+
+    vagrant
+  ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # The X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    # xkbOptions = "eurosign:e";
+
+    # Enable the KDE Desktop Environment.
+    # services.xserver.displayManager.kdm.enable = true;
+    desktopManager.kde4.enable = true;
+
+    synaptics = {
+      enable = true;
+      tapButtons = false;
+      twoFingerScroll = true;
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.chris = {
+    name = "chris";
+    group = "users";
+    extraGroups = [ "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" ];
+    createHome = true;
+    uid = 1000;
+    home = "/home/chris";
+    shell = "/run/current-system/sw/bin/fish";
+  };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "15.09";
+
+}
